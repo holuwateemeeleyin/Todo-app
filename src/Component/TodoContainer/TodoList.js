8bc88c21c814162efcdfb9f85047af3f73518f80
unknown <abegundeosamuel@gmail.com>
2022-02-24 13:14:18 +0100

    fix: made easier to read

--- src/Component/TodoContainer/TodoList.js
+++ src/Component/TodoContainer/TodoList.js
@@ -2,12 +2,24 @@ import React from 'react'
 import TodoItem from './item';
 
 export default function TodoList(props) {
-    const {todo} = props
+    const { todos, onChangeHandler } = props
+    console.log(props);
     return (
         <div className='todoList-container'>
-            <TodoItem
+            <ul>
+                {
+                    todos.map((todo) => (
+                        <TodoItem
+                            key={todo.id}
+                            todo={todo}
+                            onChangeHandler={onChangeHandler}
+                        />
+                    ))
+                }
+            </ul>
+            {/* <TodoItem
                 todo={todo}
-            />
+            /> */}
         </div>
     )
 }
