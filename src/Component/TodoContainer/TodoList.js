6c29806864e9c56a188d83c7139c4e8913290c31
unknown <abegundeosamuel@gmail.com>
2022-02-24 13:46:28 +0100

    passed delete todo button

--- src/Component/TodoContainer/TodoList.js
+++ src/Component/TodoContainer/TodoList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import TodoItem from './item';
 
 export default function TodoList(props) {
-    const { todos, onChangeHandler } = props
+    const { todos, onChangeHandler, deleteTodo } = props
     console.log(props);
     return (
         <div className='todoList-container'>
@@ -13,6 +13,7 @@ export default function TodoList(props) {
                             key={todo.id}
                             todo={todo}
                             onChangeHandler={onChangeHandler}
+                            deleteTodo = {deleteTodo}
                         />
                     ))
                 }
