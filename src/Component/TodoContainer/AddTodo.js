fb8e96d169a3f4200ddd86952ecd53f54ad9f8f8
unknown <abegundeosamuel@gmail.com>
2022-02-24 23:28:00 +0100

    fix: removal of whitespace from both sides of the string

--- src/Component/TodoContainer/AddTodo.js
+++ src/Component/TodoContainer/AddTodo.js
@@ -1,35 +1,38 @@
 import React, { Component } from 'react'
 
 export default class AddTodo extends Component {
-    state= {
-        title:''
+    state = {
+        title: ''
     }
 
-    handleOnChangeTitle =(e)=> {
+    handleOnChangeTitle = (e) => {
         this.setState({
-            title:e.target.value
+            title: e.target.value
         })
     }
-    submitForm=(e)=> {
+    submitForm = (e) => {
         e.preventDefault()
         // console.log(this.state.title);
-        this.props.addTodo(this.state.title)
-        this.setState({
-            title:''
-        })
+        if (this.state.title.trim()) { //for removing whitespace
+            this.props.addTodo(this.state.title)
+            this.setState({
+                title: ''
+            })
+        } else {
+            alert('You have to add an item')
+        }
     }
 
-  render() {
-      const { addTodo } = this.props
-    return (
-      <form onSubmit={this.submitForm}>
-          <input 
-            type='text' 
-            value={this.state.title}
-            onChange={this.handleOnChangeTitle}
-        />
-          <button>Add todo</button>
-      </form>
-    )
-  }
+    render() {
+        return (
+            <form onSubmit={this.submitForm}>
+                <input
+                    type='text'
+                    value={this.state.title}
+                    onChange={this.handleOnChangeTitle}
+                />
+                <button>Add todo</button>
+            </form>
+        )
+    }
 }
