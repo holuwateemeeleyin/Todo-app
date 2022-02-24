b5d418fb9bc153a47c880b6540ad978d9fba7026
unknown <abegundeosamuel@gmail.com>
2022-02-24 13:16:23 +0100

    passed props to change the state of the checkbox

--- src/Component/TodoContainer/item.js
+++ src/Component/TodoContainer/item.js
@@ -1,21 +1,17 @@
 import React from 'react'
 
 export default function item(props) {
-    const {todo} = props
-  return (
-    <div className='todoItem'>
-        {
-            todo.map((todoItem)=> (
-                <ul key={todoItem.id}>
-                    <li> 
-                        <input type='checkbox' 
-                        checked={todoItem.completed}
-                        onChange={()=>console.log('checked')}
-                        />
-                        {todoItem.title}</li>
-                </ul>
-            ))
-        }
-    </div>
-  )
+    const { todo, onChangeHandler } = props
+    return (
+        <div className='todoItem'>
+            <li>
+                <input type='checkbox'
+                    checked={todo.completed}
+                    onChange={() => onChangeHandler(todo.id)}
+
+                />
+                {todo.title}
+            </li>
+        </div>
+    )
 }
