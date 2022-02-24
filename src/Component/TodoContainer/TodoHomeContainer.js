af9b1f215c71dbc62dfdea1135b8ed77744ef4db
unknown <abegundeosamuel@gmail.com>
2022-02-24 13:11:58 +0100

    made changes to update the state

--- src/Component/TodoContainer/TodoHomeContainer.js
+++ src/Component/TodoContainer/TodoHomeContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import TodoList from './TodoList'
 export default class TodoHomeContainer extends Component {
     state = {
-        todo: [
+        todos: [
             {
                 id:1,
                 title: 'Outing with the Kids',
@@ -25,13 +25,29 @@ export default class TodoHomeContainer extends Component {
             }
         ]
     }
+
+    onChangeHandler = (id)=> {
+        // Checking if todo.id is the same with the clicked id,
+        // If it is the same flipped the true to false
+        // we have to do it by first looping through the data
+        this.setState({
+            todos: this.state.todos.map((todo) => {
+                if(todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo
+            })
+        })
+        // console.log('clicked', id);
+    }
   render() {
-      const {todo} = this.state
+      const {todos} = this.state
     return (
     <div className='TodoHomeContainer'>
           <h1>Todo</h1>
           <TodoList
-            todo ={todo}
+            todos ={todos}
+            onChangeHandler={this.onChangeHandler}
           />
     </div>
     )
