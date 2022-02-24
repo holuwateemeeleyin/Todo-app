f8c088ec2a20cd61a52cb74756fb38b531e7f0e1
unknown <abegundeosamuel@gmail.com>
2022-02-24 13:47:16 +0100

    added a button to delete todo items

--- src/Component/TodoContainer/item.js
+++ src/Component/TodoContainer/item.js
@@ -1,17 +1,18 @@
 import React from 'react'
 
 export default function item(props) {
-    const { todo, onChangeHandler } = props
+    const { todo, onChangeHandler, deleteTodo } = props
     return (
         <div className='todoItem'>
             <li>
                 <input type='checkbox'
                     checked={todo.completed}
                     onChange={() => onChangeHandler(todo.id)}
-
                 />
                 {todo.title}
+                <button onClick={()=>deleteTodo(todo.id)}>Delete</button>
             </li>
+            
         </div>
     )
 }
