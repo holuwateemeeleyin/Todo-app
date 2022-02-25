e6862ccd31a4fdff56ac887eae11ba04aaf79543
unknown <abegundeosamuel@gmail.com>
2022-02-25 23:57:55 +0100

    add functionality to completed todo

--- src/Component/TodoContainer/item.js
+++ src/Component/TodoContainer/item.js
@@ -1,18 +1,32 @@
 import React from 'react'
+import { MdDeleteForever } from 'react-icons/md'
+
 
 export default function item(props) {
+
     const { todo, onChangeHandler, deleteTodo } = props
+
+    const completedStyle = {
+        textDecoration: 'line-through',
+        color: 'green',
+        fontStyle: "italic",
+        opacity: 0.4,
+    }
     return (
-        <div className='todoItem'>
-            <li>
-                <input type='checkbox'
-                    checked={todo.completed}
-                    onChange={() => onChangeHandler(todo.id)}
-                />
+        <li className='todoItem'>
+            <input type='checkbox'
+                className='checkbox'
+                checked={todo.completed}
+                onChange={() => onChangeHandler(todo.id)}
+            />
+            {/* {todo.title} */}
+            <button onClick={() => deleteTodo(todo.id)} className='delete-button'>
+                <MdDeleteForever size='20' color='red' />
+            </button>
+            <span style={todo.completed ? completedStyle : null}>
                 {todo.title}
-                <button onClick={()=>deleteTodo(todo.id)}>Delete</button>
-            </li>
-            
-        </div>
+            </span>
+        </li>
+
     )
 }
