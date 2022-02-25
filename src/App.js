529977763797980885bb1398c28e51c74d3033ab
unknown <abegundeosamuel@gmail.com>
2022-02-25 22:01:17 +0100

    imported style

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import NotFound from './Component/notFound'
 import TodoHomeContainer from './Component/TodoContainer/TodoHomeContainer'
+import './App.css'
 
 const App = () => {
   return (
-    <div className='app__container'>
+    <div className='app_container'>
       <Routes>
         <Route exact path='/' element={<TodoHomeContainer />} />
       </Routes>
