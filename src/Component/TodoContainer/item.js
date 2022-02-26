6deb3597e452f841c1045fa7fb3541af3dbf9604
unknown <abegundeosamuel@gmail.com>
2022-02-26 22:37:13 +0100

    change to state based component

--- src/Component/TodoContainer/item.js
+++ src/Component/TodoContainer/item.js
@@ -1,32 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { MdDeleteForever } from 'react-icons/md'
 
+export default class item extends Component {
+    render() {
 
-export default function item(props) {
+        const completedStyle = {
+            textDecoration: 'line-through',
+            color: 'green',
+            fontStyle: "italic",
+            opacity: 0.4,
+        }
+        const { todo, onChangeHandler, deleteTodo } = this.props
+        return (
+            <li className='todoItem'>
+                <input type='checkbox'
+                    className='checkbox'
+                    checked={todo.completed}
+                    onChange={() => onChangeHandler(todo.id)}
+                />
+                {/* {todo.title} */}
+                <button onClick={() => deleteTodo(todo.id)} className='delete-button'>
+                    <MdDeleteForever size='20' color='red' />
+                </button>
+                {/* If the todo is completed, the sytling will be true. and if not the
+        will be normal */}
+                <span style={todo.completed ? completedStyle : null}>
+                    {todo.title}
+                </span>
+            </li>
 
-    const { todo, onChangeHandler, deleteTodo } = props
-
-    const completedStyle = {
-        textDecoration: 'line-through',
-        color: 'green',
-        fontStyle: "italic",
-        opacity: 0.4,
+        )
     }
-    return (
-        <li className='todoItem'>
-            <input type='checkbox'
-                className='checkbox'
-                checked={todo.completed}
-                onChange={() => onChangeHandler(todo.id)}
-            />
-            {/* {todo.title} */}
-            <button onClick={() => deleteTodo(todo.id)} className='delete-button'>
-                <MdDeleteForever size='20' color='red' />
-            </button>
-            <span style={todo.completed ? completedStyle : null}>
-                {todo.title}
-            </span>
-        </li>
-
-    )
 }
